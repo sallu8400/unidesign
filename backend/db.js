5e8eb92a92a97d912564d68e9c85b11e6835516d
sallu8400 <salmanshaikh8400@gmail.com>
2025-12-05 17:58:09 +0530

    db changes

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,18 @@
+// import sql from "mssql";
+// import dotenv from 'dotenv'
+// dotenv.config()
+
+// export const sqlInstance = sql;
+
+// export const config = {
+//  user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   server: process.env.DB_SERVER,
+//   database: process.env.DB_NAME,
+//   options: { encrypt: false, trustServerCertificate: true }
+// };
+
+
 import sql from "mssql";
 import dotenv from 'dotenv'
 dotenv.config()
@@ -5,9 +20,16 @@ dotenv.config()
 export const sqlInstance = sql;
 
 export const config = {
- user: process.env.DB_USER,
+  user: process.env.DB_USER,
   password: process.env.DB_PASS,
   server: process.env.DB_SERVER,
   database: process.env.DB_NAME,
-  options: { encrypt: false, trustServerCertificate: true }
+  
+
+  port: parseInt(process.env.DB_PORT), 
+
+  options: { 
+    encrypt: false, 
+    trustServerCertificate: true 
+  }
 };
